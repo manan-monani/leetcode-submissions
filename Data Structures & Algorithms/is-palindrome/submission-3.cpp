c36b1981997c4084aa4e88fbf12510fef64e1ff7
Manan Monani <155767948+manan-monani@users.noreply.github.com>
2026-05-23 20:11:54 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int r=s.lenght()-1;
+
+        for(int l=0;l<r;){
+            if(isalnum(s[l])) {
+                l++;
+                continue;
+
+            }
+            if(isalnum(s[r])){
+                r--;
+                continue;
+            }
+            if(tolower(s[l]))!=tolower(s[r]) return false;
+                l++;
+                r--;
+        }
+            return true;   
+    }
+};
