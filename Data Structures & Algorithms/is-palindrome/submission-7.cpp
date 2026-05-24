e9e26af75861682e6eef975ed32a58505528a59b
Manan Monani <155767948+manan-monani@users.noreply.github.com>
2026-05-24 12:21:52 +0530

    Add: is-palindrome - submission-7

---
+++ Data Structures & Algorithms/is-palindrome/submission-7.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:
+    bool ispalindrome(string s){
+        int l=0,r=s.length()-1;
+
+        while (l<r)[
+            while(l<r && !=isit_achar(s[l])){
+                l++;
+            }
+            while(l<r && !=isit_achar(s[r])){
+                r--;
+            }
+            if(tolower(s[l])!=tolower(s[r])){
+                return false;
+            }
+            l++;r--;
+        ]
+    }
+
+    bool isit_achar(char c){
+        return (c>='A'&&c<='Z' || c>='a'&&c<='z' || c>='0'&&c<='9');
+    }
+}
