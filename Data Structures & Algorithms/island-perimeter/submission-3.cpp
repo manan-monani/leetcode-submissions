1053f49a5f542f87a5d131cc03bf97fe4bca0eae
Manan Monani <155767948+manan-monani@users.noreply.github.com>
2026-05-27 13:59:35 +0530

    Add: island-perimeter - submission-3

---
+++ Data Structures & Algorithms/island-perimeter/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+        vector<vector<int>> grid;
+        vector<vector<int>> visited;
+        int rows,cols;
+
+        int dfs(int r , int c){
+            if(r<0||r>=rows ||c<0 ||c>=cols ||grid[r][c] == 0) return 1;
+
+            if(visited[r][c]) return 0;
+
+            visited[r][c] = true;
+            return dfs(i-1,j) + dfs(i+1,j) +dfs(i,j-1) +dfs(i,j+1) ; 
+        }
+
+
+    int islandPerimeter(vector<vector<int>>& grid) {
+        this->grid=grid;
+
+        rows = grid.size();
+        cols = grid[0].size();
+
+        visited = vector<vector<bool>>(rows, vector<bool>(cols, false));
+
+            for(int j=0;j<cols;++j){
+                if(grid[i][j] == 1){
+                    return dfs(i,j);
+                }
+            }
+        }
+        return 0;
+    }
+};
